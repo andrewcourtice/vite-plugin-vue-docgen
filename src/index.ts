6218524d2e9bd61e2a1dbe1e88fb9a357b9ac56c
Remy <muryoh@gmail.com>
2022-04-19 15:43:43 +0200

    feat: support options.include/exclude
    They are more standard in the rollup/vite plugin ecosystem
    My usecase is excluding things, and unfortunately having a
    single pattern does not cut it because I am in a browser environment
    and Safari does not support negative lookahead in regexps

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,44 @@ import {
     parse,
     DocGenOptions
 } from 'vue-docgen-api';
+import { createFilter, FilterPattern } from '@rollup/pluginutils';
 
 import type {
     Plugin
 } from 'vite';
 
 export interface Options {
+    /** @deprecated please use `include` instead */
     pattern?: RegExp;
+    include?: FilterPattern;
+    exclude?: FilterPattern;
     injectAt?: string;
     docgenOptions?: DocGenOptions;
 }
 
 export default function(options?: Options): Plugin {
     const {
-        pattern,
         injectAt,
+        include,
+        exclude,
         docgenOptions
     } = {
-        pattern: /\.vue$/,
+        include: options?.include || options?.pattern || /\.vue$/,
         injectAt: '__docgenInfo',
         ...options
     };
+
+    var filter = createFilter(include, exclude);
 
     return {
         name: 'vite-plugin-vue-docgen',
         enforce: 'post',
 
         async transform(source, id, ssr) {
-            if (!pattern.test(id)) {
+            if (!filter(id)) {
                 return;
             }
-
+            
             const metaData = await parse(id, docgenOptions);
             const metaSource = JSON.stringify(metaData);
 
